4ebe4f972e73d8407344ec0277630e21f4300b2f
Ikechukwu <iyke497@gmail.com>
2025-04-28 17:03:57 +0100

    Instruct dashboard bp template, add dashboard.html

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -2,10 +2,10 @@ from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from app.wallet.services import get_portfolio_data
 
-bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
+bp = Blueprint('dashboard', __name__, url_prefix='/dashboard', template_folder='templates')
 
 @bp.route('')
 @login_required
-def index(): # import db from your extensions
+def index():
     data = get_portfolio_data(current_user.id)
     return render_template('dashboard/dashboard.html', portfolio=data)
