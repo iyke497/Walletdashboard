9e21b9dcca9bbf0b661d95017747e310bae7c4b8
Ikechukwu <iyke497@gmail.com>
2025-05-06 09:58:53 +0100

    refactor trade ui, add custom css, config blueprint

--- app/trading/__init__.py
+++ app/trading/__init__.py
@@ -2,6 +2,8 @@ from flask import Blueprint
 
 trading_bp = Blueprint('trading', __name__, 
                       url_prefix='/trading',
-                      template_folder='templates')
+                      template_folder='templates',
+                      static_folder='static',
+                      static_url_path='')
 
 from . import routes
