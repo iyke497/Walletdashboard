2dbd7359e1366339bb5340720e4eb3fc93aac551
Ikechukwu <iyke497@gmail.com>
2025-05-13 16:45:02 +0100

    Modify auth templates to match vuexy

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -10,6 +10,7 @@ class RegistrationForm(FlaskForm):
     confirm  = PasswordField('Confirm Password',
                              validators=[DataRequired(), EqualTo('password')])
     submit   = SubmitField('Register')
+    agree_terms = BooleanField('I agree to privacy policy & terms', validators=[DataRequired()])
 
     def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
